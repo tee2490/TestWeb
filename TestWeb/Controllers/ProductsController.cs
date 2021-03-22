b42c71dd8fabf184cf0e647b740023c248333de7
[Teeradet] <[teeradetk@hotmail.com]>
2021-03-22 17:59:17 +0700

    a1

--- TestWeb/Controllers/ProductsController.cs
+++ TestWeb/Controllers/ProductsController.cs
@@ -20,9 +20,7 @@ namespace TestWeb.Controllers
             return View(db.Product.ToList());
            //123
            //666
-           //888
-           //999
-           //100000
+           //54321
         }
 
         // GET: Products/Details/5
